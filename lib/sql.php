10498b1f7a400ab0fa5a50b6aaf99fbeb632104e
GT3CH1 <gavinpease@gmail.com>
2021-05-10 17:44:39 -0600

    cleanup sql backend

--- lib/sql.php
+++ lib/sql.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
     How to use:
 
@@ -11,85 +12,59 @@
     $id = $sqlquery->get_id();
 
 */
-class doSQL{
+
+class doSQL
+{
 
     var $names = array();
     var $gpios = array();
     var $times = array();
-	var $days = array();
-    var $id = array();
-    
-    function set_id($d){
-        $this->id = $d;
-    }
-    function set_names($a){
-        $this->names = $a;
-    }
-    function set_gpios($e){
-        $this->gpios = $e;
-    }
-    function set_times($b){
-        $this->times = $b;
-    }
-	function set_days($c){
-		$this->days = $c;
-	}
+    var $days = array();
+    var $ids = array();
 
-    function get_id(){
-        return $this->id;
-    }
-    function get_names(){
-        return $this->names;
-    }
-    function get_gpios(){
-        return $this->gpios;
-    }
-    function get_times(){
-        return $this->times;
-    }
-	function get_days(){
-		return $this->days;
-	}
+    private $servername;
+    private $username;
+    private $password;
+    private $dbname;
 
-    function querySQL($query){
-        $servername = "pimation";
-        $username = "web";
-        $password = "X4vJOQVCF8WnnoJq";
-        $dbname = "SQLSprinkler";
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
+    public function __construct()
+    {
+        $this->servername = apache_getenv('SQLSPRINKLER_SQL_HOST');
+        $this->username = apache_getenv('SQLSPRINKLER_USER');
+        $this->password = apache_getenv('SQLSPRINKLER_PASS');
+        $this->dbname = apache_getenv('SQLSPRINKLER_DB');
+    }
+
+    function querySQL($query)
+    {
+        $conn = mysqli_connect($this->servername, $this->username, $this->password, $this->dbname);
         $query = mysqli_query($conn, $query);
-        if($query)
+        if ($query)
             return $query;
         return mysqli_error($conn);
     }
-    function doSQLStuff($query){
-        $a = 0;
-        $servername = "192.168.1.143";
-        $username = "web";
-        $password = "X4vJOQVCF8WnnoJq";
-        $dbname = "SQLSprinkler";
-        $conn   = mysqli_connect($servername, $username, $password, $dbname);
+
+    function doSQLStuff($query)
+    {
+        $conn = mysqli_connect($this->servername, $this->username, $this->password, $this->dbname);
         $result = mysqli_query($conn, $query);
         $newnames = array();
         $newgpios = array();
         $newtimes = array();
-		$newdays = array();
         $id = array();
-        if($result){
-            while($row = mysqli_fetch_array($result)){
+        if ($result) {
+            while ($row = mysqli_fetch_array($result)) {
                 array_push($newnames, $row['Name']);
                 array_push($newgpios, $row['GPIO']);
                 array_push($newtimes, $row['Time']);
-				array_push($newdays, $row['Days']);
                 array_push($id, $row['id']);
             }
-
-            $this->set_names($newnames);
-            $this->set_gpios($newgpios);
-            $this->set_times($newtimes);
-			$this->set_days($newdays);
-            $this->set_id($id);
+            $this->names = $newnames;
+            $this->gpios = $newgpios;
+            $this->times = $newtimes;
+            $this->ids = $id;
         }
     }
 }
- ?>
+
+?>
