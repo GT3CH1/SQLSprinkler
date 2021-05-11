75c1591b4826472e0b52df0241218b1245533312
GT3CH1 <gavinpease@gmail.com>
2021-05-10 18:13:35 -0600

    de-php'd index

--- js/sprinkler.js
+++ js/sprinkler.js
@@ -1,32 +1,40 @@
 var system_status = "";
 let system_enable;
+let loadTable = true;
+function getSprinklerData() {
+    $.get('lib/api.php?systemstatus', function (data, textStatus, jqXHR) {
+        system_enable = JSON.parse(data)["systemstatus"] == "1";
+        if (system_enable) {
+            $("#schedule").html("On");
+            $("#schedule-btn-txt").html("Off");
+            $("#schedule-btn").removeClass("programoff");
+            $("#schedule-btn").addClass("programon");
+        } else {
+            $("#schedule-btn").removeClass("programon");
+            $("#schedule-btn").addClass("programoff");
+            $("#schedule").html("Off");
+            $("#schedule-btn-txt").html("On");
+        }
+    });
+    $.get('lib/api.php?systems', function () {
+
+    }).done(function(data, textStatus, jqXHR) {
+        system_status = JSON.parse(data);
+        if(loadTable)
+            createTable();
+        loadTable = false;
+    });
+}
 
 function getSprinklers() {
     setInterval(function () {
-        $.get('lib/api.php?systemstatus', function (data, textStatus, jqXHR) {
-            system_enable = JSON.parse(data)["systemstatus"] == "1";
-            if (system_enable) {
-                $("#schedule").html("On");
-                $("#schedule-btn-txt").html("Off");
-                $("#schedule-btn").removeClass("programoff");
-                $("#schedule-btn").addClass("programon");
-            } else {
-                $("#schedule-btn").removeClass("programon");
-                $("#schedule-btn").addClass("programoff");
-                $("#schedule").html("Off");
-                $("#schedule-btn-txt").html("On");
-            }
-        });
-        $.get('lib/api.php?systems', function (data, textStatus, jqXHR) {
-            system_status = JSON.parse(data);
-        });
+        getSprinklerData();
         let button_id;
         let name_id;
         for (i = 0; i < system_status.length; i++) {
             button_id = system_status[i]["gpio"];
             // This line is horrid.
             name_id = (system_status[i]["status"].charAt(0).toUpperCase() + system_status[i]["status"].slice(1)) == "Off" ? "On" : "Off";
-            document.getElementById(button_id).innerHTML = "Turn " + name_id;
             document.getElementById("status-" + i).innerHTML = ((name_id == "On") ? "Off" : "On");
             if (name_id == "Off") {
                 $("#" + button_id).removeClass("systemoff")
@@ -37,10 +45,11 @@ function getSprinklers() {
             }
         }
     }, 1000);
-    $("body").delay(1750).fadeIn(250);
 }
 
 $(document).ready(function () {
+    getSprinklers();
+    $("body").delay(1750).fadeIn(250);
     $("#menuopen").click(function () {
         $("#menuopen").fadeOut(250, function () {
             $('#menunav').fadeIn(250);
@@ -74,6 +83,18 @@ $(document).ready(function () {
         });
     });
 });
+
+function createTable() {
+    console.log(system_status.length)
+    for (i = 0; i < system_status.length; i++) {
+        let sprinklerInfo = system_status[i];
+        let name = sprinklerInfo['zonename'];
+        let gpio = sprinklerInfo['gpio'];
+        let tr = "<tr><td><div class='sprinkler-info'><p class='sprinkler-name'>Zone " + (i+1) + "</p><p> " + name + " </p><p>Status: <span id='status-"+i+"'>Off</span></p> </div></td>"
+        tr += "<td><div class='sprinkler-button'><button id='"+gpio+"' name='toggle' onclick='getData("+i+"); return false' class='w3-button systemoff w3-round-xxlarge mybutton w3-center'>Turn On</button> </div></td></tr>"
+        $("#sprinklerData").append(tr);
+    }
+}
 
 function getData(index) {
     var xhttp = new XMLHttpRequest();
