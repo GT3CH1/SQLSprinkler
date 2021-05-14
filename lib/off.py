966e997c0df3c70e74e60460ea3ab0b6aa28a8cd
Gavin Pease <gavinpease@gmail.com>
2021-05-14 16:32:13 -0600

    Make off.py only turn off those used by sqlsprinkler

--- lib/off.py
+++ lib/off.py
@@ -4,10 +4,14 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-i = 1
-while i < 41:
-    GPIO.setup(i, GPIO.OUT)
-    print(i)
-    GPIO.output(i, True)
-    i=i+1
-quit()
+db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+    user="root",         # your username
+    passwd="#FiddleFire",  # your password
+    db="SQLSprinkler")        # name of the data base
+# you must create a Cursor object. It will let
+#  you execute all the queries you need
+cur = db.cursor()
+cur.execute("SELECT gpio FROM Systems;")
+# print all the first cell of all the rows
+for row in list(cur.fetchall()[0]):
+    GPIO.output(row,True)
