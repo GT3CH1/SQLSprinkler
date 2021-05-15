e655761f8604cb3d3e70e7e6a2174a6efeff19c7
Gavin Pease <gavinpease@gmail.com>
2021-05-15 09:22:45 -0600

    Made setup_sql.sh use .env variables

--- setup_sql.sh
+++ setup_sql.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-echo "Please enter username"
-read username
-echo "Please enter password"
-read password
-mysql -u $username --password=$password -e "
-CREATE DATABASE SQLSprinkler_clone;USE SQLSprinkler_clone;CREATE TABLE Systems(
+source .env
+mysql -u $SQLSPRINKLER_USER --password=$SQLSPRINKLER_PASS -h $SQLSPRINKLER_SQL_HOST -e "
+CREATE DATABASE $SQLSPRINKLER_DB;
+USE $SQLSPRINKLER_DB;
+CREATE TABLE Systems(
 Name TEXT NOT NULL,
 GPIO INT NOT NULL,
 Time INT NOT NULL,
 id INT NOT NULL AUTO_INCREMENT,
-PRIMARY KEY (id)a
+PRIMARY KEY (id)
 );
 CREATE TABLE Enabled(enabled BOOL);
 "
